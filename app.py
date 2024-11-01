50fbf17961f9424842645742de3821842e2a0ac4
mileshenkoden <mileshenkoden@gmail.com>
2024-11-01 18:47:16 +0200

    delit obgect

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from sqlalchemy.testing.suite.test_reflection import metadata
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -15,6 +16,7 @@ class Article(db.Model):
     intro = db.Column(db.String(100), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+
 
     def __repr__(self):
         return '<Article %r>' % self.id
@@ -30,10 +32,47 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc() ).all()
+    return render_template("posts.html", articles=articles)
 
-@app.route("/user/<string:name>/<int:id>")
-def user(name,id):
-    return "User page "+name + " " + str(id)
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+@app.route('/posts/<int:id>/del')
+def post_delite(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При видалені статі виникла помилка"
+
+
+
+@app.route("/create-article", methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При додавані статі виникла помилка"
+    else:
+        return render_template("create-article.html")
 
 
 if __name__ == "__main__":
